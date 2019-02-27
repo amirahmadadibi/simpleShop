ee7249c8f6406980bd669954b5c4040bc64dea98
amirahmadadibi@gmail.com <amirahmadadibi@gmail.com>
2019-02-27 22:58:32 +0330

    create basic of adapter class

---
+++ app/src/main/java/projects/com/amirahmadadibi/myapplication/ProductAdapter.java
@@ -0,0 +1,23 @@
+package projects.com.amirahmadadibi.myapplication;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class ProductAdapter extends RecyclerView.Adapter<ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
